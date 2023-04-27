a1e5e4845971465d42765ddf409329217b65b74b
malu <100313045+marialuisap@users.noreply.github.com>
2023-04-27 15:29:12 -0300

    Add files via upload

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -16,6 +16,24 @@ public class User {
     }
     public void setUserName(String name){
         this.userName = name;
-        
+    }
+
+    public void getUserPass(String pass) {
+        this.userPass = pass;
+    }
+
+    public void getUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+    //toString - Representação genérica do objeto
+    @Override
+    public String toString(){
+        return "Nome:"+this.userName+"<br>Email: "
+                +this.userEmail;
+    }
+    //métodos gerais
+    public boolean isLogged(){
+        return(this.userName.equals("malu")
+                && this.userPass.equals("senha1234"));
     }
 }
